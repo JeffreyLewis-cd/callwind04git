14ac0ed1ce4bd0b038fcf5276b2c4d189d204afe
jiangfan6 <jiangfan6@outlook.com>
2018-05-01 11:38:51 +0800

    hightLightSearchedTxt

--- src/components/callWindComponents/searchAndShow.vue
+++ src/components/callWindComponents/searchAndShow.vue
@@ -6,10 +6,10 @@
     <div class="seAndSh-dataFromBackend">
       <img src="../../assets/img/cinghoo01.png" style="margin-right: 60px;">
       <img src="../../assets/img/zf.png" style="height: 36px;margin-right: 20px">
-      <Input v-model="value13" style="width:40%">
+      <Input v-model="searchVal" style="width:40%">
 
       <span slot="prepend">征风山寨版</span>
-      <Button slot="append" icon="search" @click.native="getMockData"></Button>
+      <Button slot="append" icon="search" @click.native="searchInMockPool"></Button>
       </Input>
     </div>
 
@@ -22,25 +22,25 @@
       </RadioGroup>
     </div>
 
-   <!-- 展示数据列表-->
+    <!-- 展示数据列表-->
     <div class="seAndSh-dataList">
       <div class="seAndSh-dataList-box">
-          <div class="seAndSh-dataList-item" v-for="(mockItem, index) in mockData">
-            <div class="seAndSh-dataList-item-title">
-              <img src="../../assets/img/webpack.png"  class="seAndSh-dataList-item-img">
-              <p style="width:25%;"  class="seAndSh-dataList-item-p">
-                <span>被攻击用户名：</span><span style="font-weight: 500">{{mockItem.name}}</span>
-              </p>
-              <p style="width:45%;" class="seAndSh-dataList-item-p">
-                <span>被攻击地址：</span><span style="font-weight: 500">{{mockItem.address}}</span>
-              </p>
-              <p style="width:20%;" class="seAndSh-dataList-item-p">
-                <span>被攻击时间：</span><span style="font-weight: 500">{{mockItem.date}}</span>
-              </p>
-            </div>
-            <p class="seAndSh-dataList-item-content">{{mockItem.content}}</p>
-
+        <div class="seAndSh-dataList-item" v-for="(mockItem, index) in mockData">
+          <div class="seAndSh-dataList-item-title">
+            <img src="../../assets/img/webpack.png" class="seAndSh-dataList-item-img">
+            <p style="width:25%;" class="seAndSh-dataList-item-p">
+              <span>被攻击用户名：</span><span style="font-weight: 500" v-html="mockItem.name"></span>
+            </p>
+            <p style="width:45%;" class="seAndSh-dataList-item-p">
+              <span>被攻击地址：</span><span style="font-weight: 500" v-html="mockItem.address"></span>
+            </p>
+            <p style="width:20%;" class="seAndSh-dataList-item-p">
+              <span>被攻击时间：</span><span style="font-weight: 500" v-html="mockItem.date"></span>
+            </p>
           </div>
+          <p class="seAndSh-dataList-item-content" v-html="mockItem.content"></p>
+
+        </div>
       </div>
     </div>
   </div>
@@ -52,7 +52,7 @@
     data(){
       return {
         msg: 'hello vue',
-        value13: '',
+        searchVal: '',
         select3: 'day',
         hacker: '黑客常用手段',
         mockData: [
@@ -61,32 +61,73 @@
             date: "6666",
             content: "66",
             address: "666",
-            imgURL:""
+            imgURL: ""
           }
         ],
+        mockDataPool: [],
       }
     },
     mounted: function () {
-
+      this.getMockData();
+      console.log(this.mockDataPool);
     },
     components: {},
     methods: {
+      //生成模拟数据
       getMockData(){
         console.log("点击搜索-01");
         var Mock = require('mockjs');
         var Random = Mock.Random;
         let randomMockData = Mock.mock({
-          'tableData|60': [{
+          'tableData|500': [{
             name: ()=>Random.cname(),
             address: ()=>Random.county(true),
             date: ()=>Random.date(),
             content: ()=>Random.cparagraph(),
-            imgURL:()=>Random.image(),
+            imgURL: ()=>Random.image(),
           }]
         });
-        console.log(randomMockData);
-//        this.mockData=randomMockData;
-        this.$set(this.$data,"mockData",randomMockData.tableData)
+        this.$set(this.$data, "mockDataPool", randomMockData.tableData)
+      },
+
+      //查询搜索
+      searchInMockPool(){
+//        let searchVal=this.searchVal;
+        let searchedData = [];
+        for (let key = 0; key < this.mockDataPool.length; key++) {
+          let mockData = this.mockDataPool[key];
+          let nameIndex = mockData.name.indexOf(this.searchVal);
+          let addressIndex = mockData.address.indexOf(this.searchVal);
+          let dateIndex = mockData.date.indexOf(this.searchVal);
+          let contentIndex = mockData.content.indexOf(this.searchVal);
+
+          let matchResult = (-1 !== nameIndex) ||
+            (-1 !== addressIndex) ||
+            (-1 !== dateIndex) ||
+            (-1 !== contentIndex);
+          if (matchResult) {
+            /*关键字高亮显示*/
+            let newEle = "<span style='color:#fff;background-color:red'>" + this.searchVal + "</span>";
+            let reg = "/" + this.searchVal + "/g";
+            if (-1 !== nameIndex) {
+              mockData.name = mockData.name.replace(eval(reg), newEle);
+            }
+            else if(-1 !== addressIndex){
+              mockData.address = mockData.address.replace(eval(reg), newEle);
+            }
+            else if(-1 !== dateIndex){
+              mockData.date = mockData.date.replace(eval(reg), newEle);
+            }
+            else if(-1 !== contentIndex){
+              mockData.content = mockData.content.replace(eval(reg), newEle);
+            }
+            searchedData.unshift(this.mockDataPool[key]);
+          }
+          else {
+          }
+        }
+        console.log(searchedData);
+        this.$set(this.$data, "mockData", searchedData)
       }
     }
   }
