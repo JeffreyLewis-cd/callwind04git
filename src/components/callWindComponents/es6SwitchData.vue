c5c943ab74d87ce66daaafc27704a23745b62366
jiangfan6 <jiangfan6@outlook.com>
2018-05-05 16:44:56 +0800

    final es6 find()

--- src/components/callWindComponents/es6SwitchData.vue
+++ src/components/callWindComponents/es6SwitchData.vue
@@ -130,22 +130,22 @@
               {
                 field: "orderState",
                 label: "订单状态",
-                formatter:"pubStateToTxt",
-                stateMap:{
-                  0:"未付款",
-                  1:"收到货款-准备出库",
-                  2:"物流运输中-圆通快递",
-                  3:"到达客户附近-快递员配送中",
+                formatter: "pubStateToTxt",
+                stateMap: {
+                  0: "未付款",
+                  1: "收到货款-准备出库",
+                  2: "物流运输中-圆通快递",
+                  3: "到达客户附近-快递员配送中",
                 }
               },
               {
                 field: "goodsLevel",
                 label: "商品等级",
-                formatter:"pubStateToTxt",
-                stateMap:{
-                  0:"优等品",
-                  1:"中级品",
-                  2:"一般商品",
+                formatter: "pubStateToTxt",
+                stateMap: {
+                  0: "优等品",
+                  1: "中级品",
+                  2: "一般商品",
                 }
               },
               {
@@ -179,7 +179,7 @@
                 date: '2017-05-03',
                 name: '立柜',
                 orderState: 2,
-                goodsLevel:1,
+                goodsLevel: 1,
                 length: '120',
                 width: '60',
                 height: '150',
@@ -191,7 +191,7 @@
                 date: '2017-08-03',
                 name: '沙发',
                 orderState: 0,
-                goodsLevel:2,
+                goodsLevel: 2,
                 length: '230',
                 width: '130',
                 height: '60',
@@ -235,27 +235,29 @@
 
       /*接收后台传来的映射关系--把状态字转换成汉字*/
       pubStateToTxt(row, column, cellValue, index){
-        let oriState=cellValue;
-        let formatField=column.property;
-        let activeData=this.dataSource[this.active_index].dataTitle;
+        let oriState = cellValue;
+        let formatField = column.property;
+        let activeTitle = this.dataSource[this.active_index].dataTitle;
 
-        let formatItem=activeData.find(item=>{
-          if(item.field==formatField)
-          return item;
+        let formatItem = activeTitle.find(item=> {
+          if(item.field == formatField) {
+            return item
+          }
         });
-        let oStateMap=formatItem.stateMap;
-
-        for(let key in oStateMap){
-          if(key==oriState){
-            oriState=oStateMap[key];
+        console.log(formatItem);
+        let oStateMap = formatItem.stateMap;
+        for (let key in oStateMap) {
+          if (key == oriState) {
+            oriState = oStateMap[key];
           }
         }
+
         return oriState;
       },
 
       /*把字符串函数名转换成可执行的函数*/
       stringFuncNameToRealFunc(name){
-        let realFunc=eval('this.'+name);
+        let realFunc = eval('this.' + name);
         return realFunc;
       }
     }
